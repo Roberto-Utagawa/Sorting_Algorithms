757a0af5624be801d60447784c98a755ae165f41
Utagawa <utagawa.roberto@gmail.com>
2021-10-26 11:21:16 -0300

    Added Bubble Sort

---
+++ Bubble_Sort/Bubble_Sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int* bubble_Sort(int *array, int size)
+{
+    int i = 1, swap = 0, temp;
+
+    while (swap == 1 && i < size-1)
+    {
+        swap = 0;
+
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j+1])
+            {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+
+                swap = 1;
+            }
+        }
+        i++;
+    }
+    return array;
+}
